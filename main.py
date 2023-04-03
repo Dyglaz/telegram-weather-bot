8961373eaf24bf187e38f8d86674f81ca852a0d7
stealthebat <right.seven@yandex.ru>
2023-04-03 05:33:12 +0300

    Added docstrings

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import config
+import weather_utils
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 
+TELEGRAM_TOKEN = config.telegramBotToken
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
+
+async def start(update: Update, context) -> None:
+    """Sends a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_text(
         f"Hi, {user.username}!\n"
@@ -13,8 +17,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     )
 
 
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
+async def help_command(update: Update, context) -> None:
+    """Sends a message when the command /help is issued."""
     text = "This bot provides functions for getting weather forecast" \
            "in you city." \
            "/draw_temp - get a temperature graph in specified city\n" \
@@ -22,52 +26,36 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text(text)
 
 
-# Function to send weather data to the user
 async def send_weather_data(update, context):
+    """Sends a message with weather when the /get_weather command is issued"""
+    if len(context.args) == 0:
+        await update.message.reply_text("Specify the city")
     city = context.args[0]
-    data = get_weather_data(city)
+    data = weather_utils.get_weather_data(city)
     weather = data['weather'][0]['description']
     temp = data['main']['temp']
     await update.message.reply_text(f'The weather in {city} is {weather} with a temperature of {temp}°C.')
 
 
-# Function to draw a graph of the temperature over time for a specified city
-async def draw_temperature_graph(update, context):
+async def get_temperature_graph(update, context):
+    """Sends a temperature graph when the /get_graph command is issued"""
+    if len(context.args) == 0:
+        await update.message.reply_text("Specify the city")
     city = context.args[0]
-    url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid=11f616aaccfbd3abcdac1210710f9092&' \
-          f'units=metric&lang=ru'
-    response = requests.get(url)
-    data = response.json()
-    temps = [forecast['main']['temp'] for forecast in data['list'][:8]]
-    graph_name = f"graph-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
-    graph_path = "user_graphs/" + graph_name + ".png"
-    plt.plot(temps)
-    plt.title(f'Temperature over time in {city}')
-    plt.xlabel('Time (3 hour intervals)')
-    plt.ylabel('Temperature (°C)')
-    plt.savefig(graph_path)
-    # plt.show()
-    plt.close()
+    graph_path = weather_utils.draw_temperature_graph(city)
     await update.message.reply_photo(graph_path, caption=f"Weather graph in {city}")
 
 
-async def get_weather_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text(update.message.text)
-
-
 def main() -> None:
-    """Start the bot."""
+    """Main function to start the bot"""
     # Create the Application and pass it your bot's token.
-    application = Application.builder().token("6035452843:AAHNewpow39j7ccTc9uqEvXXUFAvUG4ZFRI").build()
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
 
     # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(CommandHandler("get_weather", send_weather_data))
-    application.add_handler(CommandHandler("draw_temp", draw_temperature_graph))
     application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e. message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, get_weather_message))
+    application.add_handler(CommandHandler("get_weather", send_weather_data))
+    application.add_handler(CommandHandler("get_graph", get_temperature_graph))
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling()
--- weather_utils.py
+++ weather_utils.py
@@ -1,35 +1,49 @@
 import config
 import requests
+import io
 import matplotlib.pyplot as plt
-from datetime import datetime
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
 
 API_KEY = config.openweatherAPI
 BASE_URL = ""
 
-# Function to draw a graph of the temperature over time for a specified city
-def draw_temperature_graph(update, context):
-    city = context.args[0]
+
+def draw_temperature_graph(city):
+    """Draw temperature graph for specified city
+
+    :param city: Target city
+    :returns: Graph picture
+    """
     url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&' \
           f'units=metric&lang=ru'
     response = requests.get(url)
     data = response.json()
     temps = [forecast['main']['temp'] for forecast in data['list'][:8]]
-    graph_name = f"graph-{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
-    graph_path = "user_graphs/" + graph_name + ".png"
-    plt.plot(temps)
-    plt.title(f'Temperature over time in {city}')
-    plt.xlabel('Time (3 hour intervals)')
-    plt.ylabel('Temperature (°C)')
-    plt.savefig(graph_path)
-    # plt.show()
-    plt.close()
-    await update.message.reply_photo(graph_path, caption=f"Weather graph in {city}")
+    fig, ax = plt.subplots()
+    ax.plot(temps)
+    ax.grid()
+    ax.set_title(f'Temperature over time in {city}')
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Temperature (°C)')
+    canvas = FigureCanvas(fig)
+    buf = io.BytesIO()
+    canvas.print_png(buf)
+    data = buf.getvalue()
+    return data
 
 
-# Function to get weather data for a specified city
 def get_weather_data(city):
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=11f616aaccfbd3abcdac1210710f9092&' \
+    """Get weather data in specified city
+
+    :param city: Target city
+    :returns: Weather data in json format
+    """
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&' \
           f'units=metric&lang=ru'
     response = requests.get(url)
     data = response.json()
     return data
+
+
+#draw_temperature_graph("Moscow")
