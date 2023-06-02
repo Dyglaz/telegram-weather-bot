109c42b491e11181de5f9d83bf6bd9ea630b6414
Dyglaz <kallkae@mail.ru>
2023-06-02 14:42:32 +0300

    Bot_02:06:23

--- config_example.py
+++ config_example.py
@@ -1,4 +1,4 @@
 """Example of configuration file containing API keys"""
 
-openweatherAPI = "yourOpenWeatherAPIkey"
-telegramBotToken = "yourTelegramBotToken"
+open_weather_token = "yourOpenWeatherAPIkey"
+tg_bot_token = "yourTelegramBotToken"
