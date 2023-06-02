6ec2b1fd94b9a96ddb13ceb2ef7ca6b4061186d9
Dyglaz <kallkae@mail.ru>
2023-06-02 19:01:52 +0300

    Bot_02:06:23

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 import datetime
 from pprint import pprint
+import io
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from telegram import InputFile
+from aiogram.types import InputFile
 from config import tg_bot_token, open_weather_token
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+
 
 bot = Bot(token=tg_bot_token)
 dp = Dispatcher(bot)
@@ -18,14 +24,42 @@ async def start_command(message: types.Message):
 
 
 @dp.message_handler(commands=["help"])
-async def start_command(message: types.Message):
-    await message.reply("Для получение прогноза погоды введите название города или страны."
-                        "/draw_temp - get a temperature graph in specified city\n"
-                        "/get_weather - get current weather in specified city")
+async def help_command(message: types.Message):
+    await message.reply("Для получение прогноза погоды введите название города или страны.\n"
+                        "/start - отобразить приветствие\n"
+                        "/get_temperature_graph - получить график температуры для указанного города или страны\n")
+
+
+@dp.message_handler(commands=["get_temperature_graph"])
+async def get_temperature_graph_command(message: types.Message):
+    try:
+        city = message.text.split()[1]
+        url = requests.get(f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={open_weather_token}'
+                        f'&units=metric&lang=ru')
+        data = url.json()
+        pprint(data['list'])
+        temps = [forecast['main']['temp'] for forecast in data['list'][:12]]
+
+        fig, ax = plt.subplots()
+        ax.plot(temps)
+        ax.grid()
+        ax.set_title(f'{city}. Температура в ближайшие 12 часов')
+        ax.set_xlabel('Время')
+        ax.set_ylabel('Температура (°C)')
+        canvas = FigureCanvas(fig)
+        buf = io.BytesIO()
+        canvas.print_png(buf)
+        buf.seek(0)
+        buf.name = f"{city}_temperature.png"
+
+        await bot.send_document(message.chat.id, InputFile(buf))
+
+    except:
+        await message.reply("\U00002620 Проверьте правильность записи названия города или страны \U00002620")
 
 
 @dp.message_handler()
-async def get_weather_command(message: types.Message):
+async def get_weather(message: types.Message):
     code_to_smile = {
         "Clear": "Ясно \U00002600",
         "Clouds": "Облачно \U00002601",
@@ -68,48 +102,8 @@ async def get_weather_command(message: types.Message):
                             f"Хорошего Вам дня!")
 
     except:
-        await message.reply("\U00002620 Проверьте правильность записи названия города \U00002620")
+        await message.reply("\U00002620 Проверьте правильность записи названия города или страны \U00002620")
 
 
 if __name__ == "__main__":
     executor.start_polling(dp)
-
-# async def start(update: Update, context) -> None:
-#     """Sends a message when the command /start is issued."""
-#     user = update.effective_user
-#     await update.message.reply_text(
-#         f"Hi, {user.username}!\n"
-#         "This bot can help you to know weather in your city\n"
-#         "You can write /help to see available commands"
-#     )
-#
-#
-# async def help_command(update: Update, context) -> None:
-#     """Sends a message when the command /help is issued."""
-#     text = "This bot provides functions for getting weather forecast" \
-#            "in you city." \
-#            "/draw_temp - get a temperature graph in specified city\n" \
-#            "/get_weather - get current weather in specified city"
-#     await update.message.reply_text(text)
-
-
-# def draw_temperature_graph(city):
-#     """Нарисовать график температуры для указанного города
-#      (параметр: Целевой город, возвращает: Графическое изображение)"""
-#
-#     url = f'https://api.openweathermap.org/data/2.5/forecast?q={city}&appid={API_KEY}&' \
-#           f'units=metric&lang=ru'
-#     response = requests.get(url)
-#     data = response.json()
-#     temps = [forecast['main']['temp'] for forecast in data['list'][:8]]
-#     fig, ax = plt.subplots()
-#     ax.plot(temps)
-#     ax.grid()
-#     ax.set_title(f'Temperature over time in {city}')
-#     ax.set_xlabel('Time')
-#     ax.set_ylabel('Temperature (°C)')
-#     canvas = FigureCanvas(fig)
-#     buf = io.BytesIO()
-#     canvas.print_png(buf)
-#     data = buf.getvalue()
-#     return data
